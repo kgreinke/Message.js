1acaeba513eb5e0061dcbd207f09e60eeab11a96
kgreinke <160787446+kgreinke@users.noreply.github.com>
2024-04-18 19:14:30 -0700

    Update

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -34,57 +34,6 @@ const UserSchema = new mongoose.Schema( {
     }
 });
 
-
-// For the perpose of password hashing.
-UserSchema.pre(save, function(next) {
-    const user = this;
-
-    // only hash the password if it has been modified (or is new)
-    if (!user.isModified('password')) return next();
-
-    // generate a salt
-    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-        if (err) return next(err);
-
-        // hash the password using our new salt
-        bcrypt.hash(user.password, salt, function(err, hash) {
-            if (err) return next(err);
-
-            // override the cleartext password with the hashed one
-            user.password = hash;
-            next();
-        });
-    });
-
-});
-
-UserSchema.methods.comparePassword = function(candidatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-        if (err) return cb(err);
-        cb(null, isMatch);
-    });
-};
-
-//Example of how to use:
-/*
-// fetch user and test password verification
-User.findOne({ username: 'jmar777' }, function(err, user) {
-    if (err) throw err;
-
-    // test a matching password
-    user.comparePassword('Password123', function(err, isMatch) {
-        if (err) throw err;
-        console.log('Password123:', isMatch); // -> Password123: true
-    });
-
-    // test a failing password
-    user.comparePassword('123Password', function(err, isMatch) {
-        if (err) throw err;
-        console.log('123Password:', isMatch); // -> 123Password: false
-    });
-});
-*/
-
 const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
