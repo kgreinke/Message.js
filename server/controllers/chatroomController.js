09d7d7633878b196df8049fbfd389bdd3473f8b1
kgreinke <160787446+kgreinke@users.noreply.github.com>
2024-04-30 17:48:43 -0700

    update

--- server/controllers/chatroomController.js
+++ server/controllers/chatroomController.js
@@ -1,24 +1,67 @@
-const ChatRoom = require('../models/chatroom-model.js');
+const ChatRoom = require('../models/chatroom-model');
+const User = require('../models/user-model');
+const Message = require('../models/message-model')
 const jsonToken  = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
 
 // Get Chatrooms Middleware
 // Route: GET /:user_id
 // Access: Need Authentication
-const getChatrooms = async(res, req, next) => {
+const getAllChats = asyncHandler( async(res, req, next) => {
     try {
-        const { chat_id } = req.params  // selectedChatId
-        const userData = req.userData   // userData of current logged in user
-        const ourUser_Id = userData.user_id;
+        ChatRoom.find( { members: { $elemMatch: { $eq: req.user._id } } })
+            .populate("members", "-password")
+            .populate("messages")
+            .sort( { updatedAt: -1 } )
+            .then( async(results) => {
+                results = await User.populate(results, {
+                    path: "messages",
+                });
+                res.status(200).send(results);
+            });
 
-        const rooms = await ChatRoom.find( {
-            chat_id: { $in: [user_id, ourUser_Id] }
-        }).sort( { createdAt: 1 } );
-
-        res.json(rooms);
-
-    } catch(err) {
-        next(err);
+    } catch(err){
+        res.status(400);
+        console.log(err.message);
     }
-}
+});
 
-module.exports = getChatrooms;
+// Create Chatroom
+// Route: POST /api/chatroom
+// Access: Need Authentication
+const createChatroom = asyncHandler( async(req, res) => {
+    if(!req.body.members || !req.body.name){
+        return res.status(400).send({
+            message: "Please Fill All Fields"
+        });
+    }
+
+    var members = JSON.parse(req.body.users);
+
+    if(members.length < 1){
+        return res.status(400).send({
+            message: "You Must Add More Than Just Yourself"
+        });
+    }
+
+    members.push(req.user)
+
+    try {
+        const chat = await ChatRoom.create( {
+            name: req.body.name,
+            members: members,
+        });
+
+        const populatedChat = await ChatRoom.findOne( { _id: chat._id } )
+            .populate("members", "-password");
+        
+        res.status(200).json(populatedChat);
+    } catch(err) {
+        res.status(400).send(err.message);
+    }
+});
+
+module.exports = {
+    getAllChats,
+    createChatroom,
+};
--- server/server.js
+++ server/server.js
@@ -7,7 +7,8 @@ const ChatApp = express();
 const ws = require('ws');
 const jsonwebtoken = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
-const ChatRoom = require('./models/chatroom-model')
+const ChatRoom = require('./models/chatroom-model');
+const Message = require('./models/message-model');
 const fs = require('fs');
 const fsPromises = require('fs').promises;
 const path = require('path');
@@ -19,7 +20,6 @@ dbConnect();
 
 ChatApp.use(express.json());
 ChatApp.use(cookieParser());
-ChatApp.use('/api/uploads'. express.static(__dirname + '/uploads'));
 ChatApp.use('/chatApp/v1/auth', require('./routes/auth'));
 ChatApp.use('/chatApp/v1/chatrooms', require('./routes/chatrooms'));
 ChatApp.use('/chatApp/v1/messages', require('./routes/messages'));
@@ -55,6 +55,97 @@ let server = ChatApp.listen(PORT, () => {
 
 
 // File Uploading
+const save = async(userFilePath, filename, bufferData) => {
+    try {
+        await fsPromises.writeFile(path.join(userFilePath, filename), bufferData);
+    } catch(err) {
+        console.log(err);
+    }
+};
+
+// Socket Server Creation
+const webSocketServer = new ws.WebSocketServer( { server } )
+webSocketServer.on('connection', (connection, req)  => {
+    console.log('Web Socket Server: Connection Aquired');
+
+    // Log of all clients currently on web socket server
+    const notifyUserLogInOut = () => {
+        [...webSocketServer.clients].forEach(client => {
+            client.send(JSON.stringify( {
+                online: [...webSocketServer.clients].map(c => ({
+                    userId: c.userId,
+                    name: c.name
+                }))
+            }))
+        })
+    }
+
+    connection.isAlive = true;
+
+    // Check For User Timeout
+    // If timeout: Terminate user connection 
+    connection.timer = setInterval( () => {
+        // Makes clients ping server on 5 second interval
+        connection.ping()
+
+        connection.deathTimer = setTimeout( () => {
+            connection.isAlive = false;
+            clearInterval(connection.timer);
+            connection.terminate();
+            notifyUserLogInOut();
+            console.log("User Terminated");
+        }, 1000)
+
+    }, 5000)
+    // Else: Do not terminate user connection
+    connection.on('pong', () => {
+        clearTimeout(connection.deathTimer)
+    });
+
+
+    // Retrieve cookie from specified connection
+    // Decode for userId and name from cookie
+    const cookie = req.headers.cookie;
+    if(cookie) {
+        const cookieToken = cookie.split(';').find(str => str.startsWith('token'));
+
+        if(cookieToken) {
+            const token = cookieToken.split('=')[1];
+
+            if(token) {
+                try{
+                    jsonwebtoken.verify(token, jsonSecret, {}, (err, userData) => {
+                        const { userId, name } = userData;
+                        // Set attributes in connection
+                        connection.userId = userId;
+                        connection.name = name;
+                    });
+                } catch(err){
+                    throw err;
+                }
+            }
+        }
+    }
+
+    // Recieve Message From Client
+    connection.on('message', async(message) => {
+        const messageData = JSON.parse(message.toString());
+        const { text, chatId } = messageData;
+
+        if( text && chatId ) {
+            // Send to DB
+            const messageDocument = await Message.create({
+                sender: connection.userId,
+                chat_id: chatId,
+                text: text
+            });
+        }
+    });
+
+    notifyUserLogInOut();
+})
+
+
 
 /*
 import express from "express";
