0670ec4718e68a75dc5db14673ddfcb30ae05cea
kgreinke <160787446+kgreinke@users.noreply.github.com>
2024-05-16 12:18:10 -0700

    Delete server/routes directory

--- server/routes/messages.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const validateUserToken = require('../utils/validateUserToken');
-const messageController = require('../controllers/messageController');
-
-router.get('/:userId', validateUserToken, messageController.getMessages);
-
-module.exports = router;
