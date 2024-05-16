72ff57e843e4b5b20d3386bbcb2dffbb570a5822
kgreinke <160787446+kgreinke@users.noreply.github.com>
2024-05-16 12:18:20 -0700

    Delete server/utility directory

--- server/utility/errorHandler.js
+++
@@ -1,9 +0,0 @@
-// Custom error handler
-const errorHandler = (statusCode, message) => {
-    const error = new Error();
-    error.statusCode = statusCode;
-    error.message = message;
-    return error
-}
-
-module.exports = { errorHandler }
